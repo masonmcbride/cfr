93ea53d204414f463953dbc9e4eb662c51d3d280
Mason McBride <mason1@berkeley.edu>
2021-01-23 16:38:39 -0800

    push files to repo

---
+++ kuhn_cfr.py
@@ -0,0 +1,117 @@
+#Monte Carlo Based Counterfactual Regret Optimization with Kuhn Poker
+#Players are each dealt 1 card from a deck of 3, actions=[check, bet] 
+
+import sys
+import numpy as np
+
+Actions = ['B', 'C'] #bet/call check/fold
+
+class InformationSet:
+    def __init__(self):
+        self.strategy_sum = np.zeros(len(Actions))
+        self.regret_sum = np.zeros(len(Actions))
+        self.num_actions = len(Actions)
+
+    def normalize(self, strategy):
+        if sum(strategy) > 0:
+            return strategy/sum(strategy)
+        else:
+            return np.ones(self.num_actions)/self.num_actions
+
+    def get_strategy(self, transition_prob):
+        strategy = np.maximum(self.regret_sum, 0)
+        strategy = self.normalize(strategy)
+
+        self.strategy_sum += transition_prob * strategy
+        return strategy
+
+    def get_average_strategy(self):
+        return self.normalize(self.strategy_sum)
+
+class KuhnPoker:
+    @staticmethod
+    def is_terminal(history):
+        return history in ['BB', 'BC', 'CC', 'CBB', 'CBC']
+
+    @staticmethod
+    def get_payoff(history, cards):
+        #get payoff with respect to current player
+        if history in ['BC', 'CBC']:
+            return 1
+        else:
+            payoff = 2 if 'B' in history else 1
+            active_player = len(history) % 2
+            player_card = cards[active_player]
+            opp_card = cards[(active_player + 1) % 2]
+            if player_card == 'K' or opp_card == 'J':
+                return payoff
+            else:
+                return -payoff
+
+class KuhnPokerTrainer:
+    def __init__(self):
+        self.infoset_map = {}
+
+    def get_information_set(self, card_plus_history):
+        if card_plus_history not in self.infoset_map:
+            self.infoset_map[card_plus_history] = InformationSet()
+        return self.infoset_map[card_plus_history]
+
+    def cfr(self, cards, history, transition_probs, active_player):
+        if KuhnPoker.is_terminal(history):
+            return KuhnPoker.get_payoff(history, cards)
+
+        my_card = cards[active_player]
+        info_set = self.get_information_set(my_card+history)
+
+        strategy = info_set.get_strategy(transition_probs[active_player])
+        opp = (active_player + 1) % 2
+        counterfactual_values = np.zeros(len(Actions))
+
+        for i, action in enumerate(Actions):
+            action_probability = strategy[i]
+
+            new_transition_probs = transition_probs.copy()
+            new_transition_probs[active_player] *= action_probability
+
+            counterfactual_values[i] = -self.cfr(cards, history+action, new_transition_probs, opp)
+
+        node_value = counterfactual_values.dot(strategy)
+        for i, action in enumerate(Actions):
+            info_set.regret_sum[i] += transition_probs[opp] * (counterfactual_values[i] - node_value)
+
+        return node_value
+
+    def train(self, iterations):
+        util = 0
+        kuhn_cards = ['J', 'Q', 'K']
+        for _ in range(iterations):
+            cards = np.random.choice(kuhn_cards, size=2, replace=False)
+            history = ''
+            transition_probs = np.ones(len(Actions))
+            util += self.cfr(cards, history, transition_probs, 0)
+
+        return util
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        iterations = 100000
+    else:
+        iterations = int(sys.argv[1])
+    np.set_printoptions(precision=2, floatmode='fixed', suppress=True)
+
+    cfr_trainer = KuhnPokerTrainer()
+    print("starting training")
+    util = cfr_trainer.train(iterations)
+
+    print(f"\nRunning Kuhn Poker chance sampling CFR for {iterations} iterations")
+    print(f"\nExpected average game value (for player 1): {(-1./18):.3f}")
+    print(f"Computed average game value               : {(util / iterations):.3f}\n")
+
+    print("We expect the bet frequency for a Jack to be between 0 and 1/3")
+    print("The bet frequency of a King should be three times the one for a Jack\n")
+
+    print(f"History  Bet  Pass")
+    for name, info_set in sorted(cfr_trainer.infoset_map.items(), key=lambda s: len(s[0])):
+        print(f"{name:3}:    {info_set.get_average_strategy()}")
+
---
+++ rps_cfr.py
@@ -0,0 +1,65 @@
+#Counterfactual Regret Minimization implementation
+#Rock Paper Scissors example
+import itertools
+import numpy as np
+
+NUM_ACTIONS = 3
+NUM_PLAYERS = 2
+ROCK, PAPER, SCISSORS = range(NUM_ACTIONS)
+
+A = np.array(list(itertools.product(range(NUM_ACTIONS), repeat=NUM_PLAYERS))) #all combinations of simul actions
+
+class Player:
+    def __init__(self, regrets_sum=np.zeros(NUM_ACTIONS)):
+        self._strategy = np.zeros(NUM_ACTIONS)
+        self.strategy_sum = np.zeros(NUM_ACTIONS)
+        self.regrets_sum = regrets_sum
+        self.number_of_updates = 0
+
+    @property
+    def strategy(self):
+        self._strategy = np.maximum(self.regrets_sum, 0)
+        self._strategy = self._strategy/sum(self._strategy) if sum(self._strategy) > 0 \
+                    else np.ones(NUM_ACTIONS)/NUM_ACTIONS
+        self.strategy_sum += self._strategy
+        return self._strategy
+
+    def payoff(self, my_action, other_action):
+        mod_3 = (my_action - other_action) % 3
+        if mod_3 == 2:
+            return -1
+        else:
+            return mod_3
+
+    def update_regrets(self, other_action, my_payoff):
+        self.regrets_sum += np.array([self.payoff(a, other_action) - my_payoff for a in range(NUM_ACTIONS)])
+
+    def update_strategy(self, my_action, other_action):
+        my_payoff = self.payoff(my_action, other_action)
+        self.update_regrets(other_action, my_payoff)
+        self.number_of_updates += 1
+    
+def get_action(strategy):
+    return np.random.choice(range(NUM_ACTIONS), p=strategy)
+
+def train(iterations, me, opp):
+    for _ in range(iterations):
+        #⟨Get regret-matched mixed-strategy actions⟩
+        my_action = get_action(me.strategy)
+        e = .34
+        r = (1-e)/2
+        opp_action = get_action([e, r, r])
+        me.update_strategy(my_action, opp_action)
+        #opp.update_strategy(opp_action, my_action)
+
+    my_optimal = me.strategy_sum / me.number_of_updates
+    #opp_optimal = opp.strategy_sum / opp.number_of_updates
+    opp_optimal = "N/A"
+    return my_optimal, opp_optimal
+
+if __name__ == '__main__':
+    me = Player()
+    opp = Player()
+    me_optimal, opp_optimal = train(1000000, me, opp)
+    print(f"my optimal strategy: {me_optimal}")
+    print(f"opponent optimal strategy: {opp_optimal}")
